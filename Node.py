ef880602e3fe596840b7b09b3685562de749494f
Bodecker DellaMaria <bojdell@gmail.com>
2015-04-16 16:53:31 -0500

    changed Node

--- Node.py
+++ Node.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import socket
 import sys
 import threading
@@ -5,66 +7,86 @@ import pickle
 
 class Node():
 	"""
-    Chord Node Class: contains functionality for a node in a Chord P2P network
-    """
+	Chord Node Class: contains functionality for a node in a Chord P2P network
+	"""
 
 	def __init__(self, nodeID, host, port):
-    	self.nodeID = nodeID
-    	self.host = host
-    	self.port = port
-    	self.finger_table = []	# contains finger pointers for this node
-    	self.predecessor = 0
-    	self.key_store = {}
+		self.nodeID = nodeID
+		self.host = host
+		self.port = port
+		self.finger_table = []	# contains finger pointers for this node
+		self.predecessor = 0
+		self.key_store = {}
 
-    def join(self, otherNodeID):
-    	pass
+		self.__start()			# open listener socket
+		print "Node " + str(nodeID) + " started"
 
-    def init_finger_table(self, otherNodeID):
-    	pass
+	def join(self, otherNodeID):
+		pass
 
-    def update_others(self, otherNodeID):
-    	pass
+	def init_finger_table(self, otherNodeID):
+		pass
 
-    def update_finger_table(self, otherNodeID, index):
-    	pass
+	def update_others(self, otherNodeID):
+		pass
 
-    # start the listen thread
-    def start(self):
-        listenerThread = threading.Thread(target=self.__listen)
-        listenerThread.setDaemon(True)
-        listenerThread.start()
+	def update_finger_table(self, otherNodeID, index):
+		pass
 
-    def __send_message(self, message, dest_host, dest_port):
-    	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    	sock.sendto(pickle.dumps(message), (dest_host, dest_port))
-    	sock.close()
+	def update_finger_table_MSG(self, args):
+		pass
 
-    def __listen(self):
-    	# Create a socket that will receive all incoming messages to this node
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.settimeout(None)
-        sock.bind((self.host, self.port))
+	def update_predecessor_MSG(self, args):
+		pass
 
-        # loop forever, listening to incoming data
-        while(1):
-            # Receive incoming data
-            received = sock.recv(1024)
+	def transfer_keys_MSG(self, args):
+		pass
 
-            # if we've received data, process it
-            if received:
-                self.__process_message(pickle.loads(received))
+	# for testing purposes only
+	def send_message(self, message, dest_host, dest_port):
+		self.__send_message(message, dest_host, dest_port)
 
-    def __process_message(self, message):
-    	fn = getattr(self, message.function)	# get fn pointer
-    	fn(message.args)						# call fn
+	# for testing purposes only
+	def hi(self, args):
+		print str(self.nodeID) + " - hi :)"
+
+	# start the listen thread
+	def __start(self):
+		listenerThread = threading.Thread(target=self.__listen)
+		listenerThread.setDaemon(True)
+		listenerThread.start()
+
+	def __send_message(self, message, dest_host, dest_port):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.sendto(pickle.dumps(message), (dest_host, dest_port))
+		sock.close()
+
+	def __listen(self):
+		# Create a socket that will receive all incoming messages to this node
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		sock.settimeout(None)
+		sock.bind((self.host, self.port))
+
+		# loop forever, listening to incoming data
+		while(1):
+			# Receive incoming data
+			received = sock.recv(1024)
+
+			# if we've received data, process it
+			if received:
+				self.__process_message(pickle.loads(received))
+
+	def __process_message(self, message):
+		fn = getattr(self, message.function)	# get fn pointer
+		fn(message.args)						# call fn
 
 
 class Message():
 	"""
-    Class that is used to communicate between nodes in the Chord network
-    """
+	Class that is used to communicate between nodes in the Chord network
+	"""
 
-    def __init__(self, function, args):
-    	self.function = str(function).lower() if function else None
-    	self.args = args if args else None
+	def __init__(self, function, args):
+		self.function = str(function).lower() if function else None
+		self.args = args if args else None
